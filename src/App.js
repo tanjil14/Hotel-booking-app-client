118c801ef0c587610364e9fd561cb34bfc29ff4e
tanjil114 <tanjil1@cse.pstu.ac.bd>
2022-06-08 15:46:37 +0600

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./app.css";
 import Home from "./pages/home/Home";
 import Hotel from "./pages/hotel/Hotel";
 import List from "./pages/list/List";
@@ -10,11 +7,11 @@ import List from "./pages/list/List";
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/hotels" element={<List/>}/>
-      <Route path="/hotels/:id" element={<Hotel/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<Hotel />} />
+      </Routes>
     </BrowserRouter>
   );
 }
