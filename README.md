a8e8d1cb18fb7ed63fbbd050a316b27aa003b5ee
tanjil114 <tanjil1@cse.pstu.ac.bd>
2022-06-08 12:21:05 +0600

    clear all unnecessary files

---
+++ README.md
@@ -0,0 +1,1 @@
+## Welcome to my booking app
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Welcome to my new booking app</h2>
+      <h2>Welcome to my new booking app</h2>
+    </div>
+  );
+}
+
+export default App;
