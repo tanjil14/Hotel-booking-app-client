118fc4d2eb00a4d4057c75a27955452e0f902f20
tanjil114 <tanjil1@cse.pstu.ac.bd>
2022-06-09 10:24:16 +0600

    adding property components in home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import Header from '../../components/header/Header';
-import Navbar from '../../components/navbar/Navbar';
-
+import React from "react";
+import "./home.css"
+import Featured from "../../components/featured/Featured";
+import Header from "../../components/header/Header";
+import Navbar from "../../components/navbar/Navbar";
+import PropertyList from "../../components/propertyList/PropertyList";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 const Home = () => {
-    return (
-        <div>
-            <Navbar/>
-            <Header/>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList/>
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties/>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
